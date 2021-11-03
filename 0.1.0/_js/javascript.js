769660523a39080aa6a9b0f4a3438d4dd219b83b
pellegrinnif <pellegrinnif@gmail.com>
2021-11-03 12:11:16 -0300

    implementação de novas funcionalidades do protótipo

--- 0.1.0/_js/javascript.js
+++ 0.1.0/_js/javascript.js
@@ -1,25 +1,37 @@
-function validateLogonCriarConta(){
+function validarLogon() {
+    if (document.getElementById("logonUser") == "" || document.getElementById("logonPassword") == "")
+        return false;
+    else {
+        window.open("meusProjetos.html");
+        //    window.location.href = "http://www.w3scholls.com";
+        return true;
+    }
+}
+
+function validateLogonCriarConta() {
     passwd = formularioData.logonFormPasswd.value;
     passwdConfirm = formularioData.logonFormPasswdConfirm.value;
     inputCode = formularioData.iConfirmCode.value;
     iCodeVisible = formularioData.iConfirmCode.offsetParent;
 
-    if ((iCodeVisible != null) && (inputCode == "")){
+    if ((iCodeVisible != null) && (inputCode == "")) {
         alert("Código de verificação inválido !");
         return false;
-    } 
-    if ((iCodeVisible != null) && (inputCode != "")){
-        window.open("home.html","_self");
+    }
+    if ((iCodeVisible != null) && (inputCode != "")) {
+        //        window.open("minhasCompetenciasRecursos.html");
+        window.location.href = "http://www.w3scholls.com";
         return true;
-    } 
-        
-    if ((iCodeVisible == null) && (logonValidaSenha(passwd,passwdConfirm))){
-//        window.open("http://www.w3schools.com","_blank");
+    }
+
+    if ((iCodeVisible == null) && (logonValidaSenha(passwd, passwdConfirm))) {
+        //        window.open("http://www.w3schools.com","_blank");
         document.getElementById("dConfirmCode").style.display = "block";
     }
     return false;
 }
-function logonValidaSenha(pwd,pwdc){
+
+function logonValidaSenha(pwd, pwdc) {
     if (pwd == pwdc) {
         alert("Senha Válida !");
         return true;
